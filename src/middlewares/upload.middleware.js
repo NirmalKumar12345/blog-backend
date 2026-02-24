dab3929404b9a9235ad2cc90a84f1714b3fb6154
nirmal <126151898+NirmalKumar12345@users.noreply.github.com>
2026-02-24 13:40:33 +0530

    cloudfile implement

--- src/middlewares/upload.middleware.js
+++ src/middlewares/upload.middleware.js
@@ -1,27 +1,15 @@
-import { error } from "console";
 import multer from "multer";
-import path from "path";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
+import cloudinary from "../config/cloudinary.js";
 
-const storage= multer.diskStorage({
-    destination: function(req,file,cb){
-        cb(null,"uploads/");
+const storage = new CloudinaryStorage({
+    cloudinary,
+    params: {
+        folder: "blog_uploads",
+        allowed_formats: ["jpg", "png", "jpeg", "svg", "webp"],
     },
-    filename: function(req,file,cb){
-        const uniqueName = Date.now() + path.extname(file.originalname);
-        cb(null,uniqueName);
-    }
 });
 
-const fileFilter = (req,file,cb)=>{
-    if(file.mimetype.startsWith("image/")){
-        cb(null,true)
-    }else{
-        cb(new Error("Only image files are upload"),false)
-    }
-}
-
-export const upload =multer({
-    storage,
-    fileFilter,
-    limits: {fileSize: 2 * 1024 * 1024}
+export const upload = multer({
+    storage
 })

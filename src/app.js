751ef1baed74167652efaf9c25df1f4d5e96fec6
Nirmal Kumar G <126151898+NirmalKumar12345@users.noreply.github.com>
2026-05-15 19:58:40 +0530

    helth api added

--- src/app.js
+++ src/app.js
@@ -30,6 +30,7 @@ app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/post', Post);
 app.use('/uploads', express.static("uploads"))
+app.get("/api/health", (req, res) => { res.status(200).json({ status: "ok" }); });
 app.use((err, req, res, next) => {
   console.error(err);
   const status = err.status || 500;
